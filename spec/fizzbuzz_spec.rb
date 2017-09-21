660e2eeb69449bea46b894742974fa3b860465f4
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-09-21 12:32:51 +0100

    Solved for normal number. Tests written and passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,15 +2,37 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  it 'returns "fizz" when passed a multiple of 3' do
-    expect(fizzbuzz(9)).to eq 'fizz'
+  context 'returns "fizz" when passed a multiple of 3' do
+    it 'returns fizz when given 9' do
+      expect(fizzbuzz(9)).to eq 'fizz'
+    end
   end
 
-  it 'returns "buzz" when passed a multiple of 5' do
-    expect(fizzbuzz(10)).to eq 'buzz'
+  context 'returns "buzz" when passed a multiple of 5' do
+    it 'returns buzz when given 5' do
+      expect(fizzbuzz(10)).to eq 'buzz'
+    end
+
+    it 'returns buzz when given 10' do
+      expect(fizzbuzz(10)).to eq 'buzz'
+    end
   end
 
-  it 'returns "fizzbuzz" when passed a multiple of 3 & 5' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  context 'returns "fizzbuzz" when passed a multiple of 3 & 5' do
+    it 'returns fizzbuzz when given 15' do
+      expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
   end
+
+  context 'returns number when passed a number that is not a multiple of 3 & 5' do
+    it 'returns 1 when given 1' do
+      expect(fizzbuzz(1)).to eq 1
+    end
+
+    it 'returns 2 when given 2' do
+      expect(fizzbuzz(2)).to eq 2
+    end
+  end
+
+
 end
